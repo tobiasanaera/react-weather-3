17c712f98362ef16c171a200666de2ce7ec776df
tobiasanaera <131626785+tobiasanaera@users.noreply.github.com>
2023-10-09 22:19:06 +1300

    apply convert to C to F

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setunit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setunit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setunit("celsius");
+  }
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>{" "}
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
